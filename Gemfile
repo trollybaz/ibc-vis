d22f2de59a15764414fea3b84c74eac7a1a5f50f
Frank Chiu <frank@manymoon.com>
2012-02-20 17:52:36 -0800

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,23 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.1.0'
+
+gem 'pg'
+gem 'rake', '0.9.2'
+gem 'devise'
+
+# Gems used only for assets and not required
+# in production environments by default.
+group :assets do
+  gem 'sass-rails', "  ~> 3.1.0"
+  gem 'coffee-rails', "~> 3.1.0"
+  gem 'uglifier'
+end
+
+gem 'jquery-rails'
+
+
+group :test do
+  # Pretty printed test output
+  gem 'turn', :require => false
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IbcVis::Application.config.secret_token = 'a5922e87d0ea89a5130c78a4582bf3dc3588ed2fb23d878be6c21ca4e1cf961516bec35a1178d15ee30c6d3aa2ecf249b30a9d105da1615226bbd34be889f8a3'
